0312d77b17dc65e5f9403c9439502b4b3df9dd58
Nathanael Berliano <nael.ano101@gmail.com>
2024-02-05 15:21:33 +0700

    update logout test

--- basictest/test_logout.py
+++ basictest/test_logout.py
@@ -27,18 +27,6 @@ class LogoutTestCase(unittest.TestCase):
         self.browser.find_element(By.ID, 'inputPassword').send_keys('nimda666!')
         self.browser.find_element(By.TAG_NAME, 'button').click()
 
-    def index_page(self):
-        expected_result = "admin"
-        actual_result = self.browser.find_element(By.XPATH, "//h2[contains(text(),'Halo,')]").text.split(', ')[1]
-        self.assertIn(expected_result, actual_result)
-
-    def logout(self):
-        self.browser.find_element(By.XPATH, "//a[contains(text(),'Sign out')]").click()
-
-        login_page_title = "Login"
-        actual_title = self.browser.title
-        self.assertEqual(login_page_title, actual_title)
-
     @classmethod
     def tearDownClass(cls):
         cls.browser.quit()
